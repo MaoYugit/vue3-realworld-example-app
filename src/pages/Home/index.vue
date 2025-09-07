c0d45f8de811d485e9b9623710b8716d14f1c358
MaoYu <2874553847@qq.com>
2025-09-07 20:40:03 +0800

    首页框架改成tailwind css

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,52 +1,53 @@
 <template>
   <div class="home-page">
     <!-- 页眉 Banner 区域 -->
-    <div class="banner">
+    <div class="bg-sky-500 text-white shadow-md py-12">
       <!-- 内容容器 -->
-      <div class="container">
+      <div class="container mx-auto text-center">
         <!-- 网站 Logo/标题 -->
-        <h1 class="logo-font">
+        <h1 class="text-5xl font-bold mb-2 drop-shadow-md">
           <!-- conduit -->
           {{ t("global.conduit") }}
         </h1>
         <!-- 网站的 Slogan/口号 -->
-        <p>
-          <!-- A place to share your knowledge. -->
+        <p class="text-xl font-light">
           {{ t("global.slogan") }}
         </p>
       </div>
     </div>
 
     <!-- 主体内容区域 -->
-    <div class="container page">
+    <div class="container mx-auto py-8">
       <!-- 行：class="row" 是一个布局类，表示这里面的内容要按“行”来组织。 -->
-      <div class="row">
+      <div class="flex flex-col lg:flex-row gap-8">
         <!-- 左侧主内容区 在中等屏幕(md)及以上占 9/12  -->
-        <div class="col-md-9">
+        <div class="w-full lg:w-3/4">
           <!-- Suspense 异步组件边界 -->
           <Suspense>
             <!-- 默认内容：这里是最核心的文章列表组件。 -->
             <ArticlesList use-global-feed use-my-feed use-tag-feed />
             <!-- #fallback 插槽：这是 Suspense 的一部分。 -->
             <template #fallback>
-              <!-- Articles are downloading...  -->
-              {{ t("article.loadingArticles") }}
+              <div class="p-4 text-gray-500 italic">
+                {{ t("article.loadingArticles") }}
+              </div>
             </template>
           </Suspense>
         </div>
 
         <!-- 右侧边栏 (占 3/12) -->
-        <div class="col-md-3">
+        <div class="w-full lg:w-1/4">
           <!-- 侧边栏容器 -->
-          <div class="sidebar">
+          <div class="bg-slate-100 p-4 rounded-lg">
             <!-- 同样使用 Suspense 来处理热门标签组件的异步加载。 -->
             <Suspense>
               <!-- 默认内容：热门标签组件。 -->
               <PopularTags />
               <!-- #fallback 插槽：在热门标签数据加载完成前显示的内容。 -->
               <template #fallback>
-                <!-- Popular tags are downloading...  -->
-                {{ t("article.loadingTags") }}
+                <div class="text-gray-500 italic">
+                  {{ t("article.loadingTags") }}
+                </div>
               </template>
             </Suspense>
           </div>
