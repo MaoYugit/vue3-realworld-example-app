081a7b0aa3359718da0f5576644f982e9d445a51
maoyu <2874553847@qq.com>
2025-09-09 18:39:10 +0800

    一个小调整

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -7,19 +7,18 @@
       <AppLink class="text-sky-500 text-2xl font-bold" name="global-feed">
         {{ t("global.conduit") }}
       </AppLink>
-
-      <!-- 4. 导航链接列表：使用 flex 布局，并为各项之间添加间距 -->
+      距
+      <!-- 4. 导航链接列表：使用 flex 布局，并为各项之间添加间-->
       <ul class="flex items-center space-x-2 md:space-x-4">
-        <li v-for="link in navLinks" :key="link.name" class="">
+        <!-- 导航链接 -->
+        <li v-for="link in navLinks" :key="link.name">
           <!-- 5. 单个导航链接：
                - 基础样式：灰色文字，提供内边距以扩大点击区域
                - 交互样式：鼠标悬停时文字颜色加深
-               - 激活样式：当路由匹配时，"active" 类会被应用，文字变为更深的颜色并加粗
                - 过渡效果：让颜色变化更平滑
           -->
           <AppLink
             class="flex items-center px-3 py-2 text-gray-500 hover:text-gray-900 transition-colors duration-300"
-            active-class="active"
             :name="link.name"
             :params="link.params"
             :aria-label="link.title"
@@ -28,6 +27,8 @@
             {{ link.title }}
           </AppLink>
         </li>
+
+        <!-- 语言切换按钮 -->
         <li>
           <LanguageSwitcher />
         </li>
@@ -53,7 +54,7 @@ interface NavLink {
   params?: Partial<RouteParams>;
   title: string;
   icon?: string;
-  display: "all" | "anonym" | "authorized";
+  display: "all" | "anonym" | "authorized"; // 什么时候展示 全部 / 未登录 / 已登录
 }
 
 const { user } = storeToRefs(useUserStore());
